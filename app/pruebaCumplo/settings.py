a47079e0e465d7b8b7bdfd1a956a06cfc1c875a6
delia <del@gmail.com>
2020-10-22 17:59:00 -0500

    listo

--- app/pruebaCumplo/settings.py
+++ app/pruebaCumplo/settings.py
@@ -10,14 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+"""<3 made with <3 by D.C."""
+
 from pathlib import Path
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=t&1j^6mwyj4s%&w=31*&37h&!0dgl6(8@*)q(4@5mnzkb-rlt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
